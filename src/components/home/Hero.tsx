dc0a525b5d2f539d3ca35f8e916d91cd60ec0db6
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-07-19 19:32:01 +0600

    update utilaty

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -3,8 +3,6 @@ const Hero = () => {
     return (
         <div>
             Hero
-            <div className="bg-yellow-300 w-48 h-48"></div>
-            <div className="bg-[#FB8F2C] w-48 h-48"></div>
         </div>
     );
 };
