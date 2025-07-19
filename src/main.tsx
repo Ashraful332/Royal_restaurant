744893927cbd3b904c283c5c72f45505fde31309
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-07-19 14:34:48 +0600

    add the page rout

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,20 @@ import ReactDOM from "react-dom/client";
 import './index.css'
 import App from './App.tsx'
 import { Toaster } from 'react-hot-toast';
+import About from "./page/about/About.tsx";
+import Blog from "./page/blog/Blog.tsx";
+import Contact from "./page/Contact/Contact.tsx";
+import Menu from "./page/menu/Menu.tsx";
+import Reservation from "./page/Reservation/Reservation.tsx";
 
 
 const router = createBrowserRouter([
   {  path: "/",  element: <App/>},
+  {  path: "/about",  element: <About/>},
+  {  path: "/blog",  element: <Blog/>},
+  {  path: "/contact",  element: <Contact/>},
+  {  path: "/menu",  element: <Menu/>},
+  {  path: "/reservation",  element: <Reservation/>},
 ]);
 
 const root = document.getElementById("root") as HTMLElement;
---
+++ src/page/Reservation/Reservation.tsx
@@ -0,0 +1,11 @@
+
+
+const Reservation = () => {
+    return (
+        <div>
+            Table Reservation
+        </div>
+    );
+};
+
+export default Reservation;
