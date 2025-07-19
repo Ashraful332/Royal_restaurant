2e684d8fd224af1383ead55eea666670aee19c22
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-07-19 17:06:38 +0600

    init nav

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -1,10 +1,28 @@
+import { Link } from "react-router";
+import Logo from "../../assets/image/Hero/logo.svg"
 
 
 const Navbar = () => {
     return (
-        <div>
-            
-        </div>
+        <nav>
+            <div>
+                <Link to='/'>Home</Link>
+                <Link to='/'>Menu</Link>
+                <Link to='/'>Blog</Link>
+                <Link to='/'>Contact</Link>
+            </div>
+            <div>
+                <img src={Logo} alt="logo" />
+            </div>
+            <div>
+                <p>
+                    11:24 we're open
+                </p>
+                <Link to='/'>
+                    Table Reservation
+                </Link>
+            </div>
+        </nav>
     );
 };
 
