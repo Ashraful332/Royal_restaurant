a4d1a4b1c70b4fdbe2cb1b7456bd435a74f29e74
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-07-20 13:45:29 +0600

    ok for now

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,9 +1,33 @@
 
 const Hero = () => {
     return (
-        <div>
-            Hero
-        </div>
+        <main>
+            <div>
+                <p>Hi, new  friend!</p>
+                <h1>We do not cook, we create your emotions!</h1>
+                <p>There's evidence that cooking, like other creative practices, can boost well-being, self-esteem, and other measures of mental health.</p>
+                <button className="hero-btn">Our Menu</button>
+            </div>
+            <div>
+                <div>
+                    <img src="" alt="" />
+                    <div>
+                        <p></p>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <img src="" alt="" />
+                    </div>
+                    <div>
+                        <img src="" alt="" />
+                    </div>
+                    <div>
+                        <img src="" alt="" />
+                    </div>
+                </div>
+            </div>
+        </main>
     );
 };
 
