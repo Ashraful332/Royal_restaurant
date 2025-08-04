7218ea4f6304f2e8086bd78598c1baf21da0282e
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-08-04 17:25:08 +0600

    the royal restaurant is add the book new

--- src/components/home/BookNow.tsx
+++ src/components/home/BookNow.tsx
@@ -2,8 +2,18 @@
 
 const BookNow = () => {
     return (
-        <div>
-            Book Now
+        <div className="flex-center mx-auto mt-high">
+            <div className="flex text-black w-[60vw] mx-auto ">
+                <div className="flex flex-col bg-primary pl-10 pt-5 w-[50%] ">
+                    <h2 className="text-4xl">Reserve ! Book Now</h2>
+                    <button className="text-xl">Set Date</button>
+                    <button className="w-[60%] bg-black text-white py-1 ">Book now</button>
+                </div>
+                <div className="w-[50%]">
+                    <img src="https://i.ibb.co/YF266N2s/638192069f6b477481e3a33f-Group-284.png" alt="coffee"
+                    className="w-auto h-auto" />
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/home/Offers.tsx
+++ src/components/home/Offers.tsx
@@ -2,8 +2,62 @@
 
 const Offers = () => {
     return (
-        <div>
-            Offers
+        <div className="flex-center mt-high">
+            <h2 className="text-6xl font-bold text-center mb-10">Our Daily Offers</h2>
+            <div>
+                <div>
+                    <div>
+                        <img src="" alt="" />
+                    </div>
+                    <div>
+                        <h3>Lunch Time</h3>
+                        <p>
+                            <span>30%</span>
+                            off
+                        </p>
+                        <p>We love food, lots of different and food, just like you.</p>
+                        <button>Order Now</button>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <div>
+                            <img src="" alt="" />
+                        </div>
+                        <div>
+                            <div>
+                                <p></p>
+                                <p></p>
+                            </div>
+                            <p></p>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <img src="" alt="" />
+                        </div>
+                        <div>
+                            <div>
+                                <p></p>
+                                <p></p>
+                            </div>
+                            <p></p>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <img src="" alt="" />
+                        </div>
+                        <div>
+                            <div>
+                                <p></p>
+                                <p></p>
+                            </div>
+                            <p></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
