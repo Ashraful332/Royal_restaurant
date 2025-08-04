b73831f74a2b81e489f7bfbeb6598d7e59a29fb4
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-08-04 16:34:41 +0600

    update

--- src/components/home/Our_Menu.tsx
+++ src/components/home/Our_Menu.tsx
@@ -2,8 +2,105 @@
 
 const Our_Menu = () => {
     return (
-        <div>
-            Our Menu
+        <div className="flex-center max-w-[1170px] mx-auto mb-high ">
+            <p>Our Menu</p>
+            <h3 className="py-5 mt-5 text-6xl mb-5 ">
+                Choose & Taste What You Like
+            </h3>
+            <p className="mb-primary w-[52%] text-center ">
+                A list of top Bangladeshi food including mains, drinks, and deserts you must try while in Bangladesh, for an authentic experience. Check now!
+            </p>
+            <div className="flex items-center gap-11 text-3xl pb-6">
+                <p>All</p>
+                <p>Appetizer</p>
+                <p>Soup</p>
+                <p>Dessert</p>
+                <p>Drinks</p>
+            </div>
+            <hr className="w-[87%] mb-12 h-[1.5px] text-[var(#FB8F2C)] bg-amber-400 brightness-50 "/>
+            <div className="flex h-[500px] ">
+                <div>
+                    <div>
+                        <div className="flex items-center">
+                            <div className="w-[100px] h-[100px] overflow-hidden object-cover relative ">
+                                <img src="https://i.ibb.co/ZRmL8Hgg/637cb9ba322617428493829d-1111.png" alt="menu" className="hover-image-menu-out" />
+                            </div>
+                            <div className="flex justify-between w-[445px] text-2xl pr-4">
+                                <p className="">Chawl Beans and Mint Burger</p>
+                                <p>$11</p>
+                            </div>
+                        </div>
+                    </div>
+                    <hr className="mt-2" />
+                    <div>
+                        <div className="flex items-center">
+                            <div className="w-[100px] h-[100px] overflow-hidden object-cover relative ">
+                                <img src="https://i.ibb.co/ZRmL8Hgg/637cb9ba322617428493829d-1111.png" alt="menu" className="hover-image-menu-out" />
+                            </div>
+                            <p className="text-2xl pr-4">Chawli Beans and Mint Burger</p>
+                        </div>
+                    </div>
+                    <hr className="mt-2" />
+                    <div>
+                        <div className="flex items-center">
+                            <div className="w-[100px] h-[100px] overflow-hidden object-cover relative ">
+                                <img src="https://i.ibb.co/ZRmL8Hgg/637cb9ba322617428493829d-1111.png" alt="menu" className="hover-image-menu-out" />
+                            </div>
+                            <p className="text-2xl pr-4">Chawli Beans and Mint Burger</p>
+                        </div>
+                    </div>
+                    <hr className="mt-2" />
+                    <div>
+                        <div className="flex items-center">
+                            <div className="w-[100px] h-[100px] overflow-hidden object-cover relative ">
+                                <img src="https://i.ibb.co/ZRmL8Hgg/637cb9ba322617428493829d-1111.png" alt="menu" className="hover-image-menu-out" />
+                            </div>
+                            <p className="text-2xl pr-4">Chawli Beans and Mint Burger</p>
+                        </div>
+                    </div>
+                    <hr className="mt-2" />
+                </div>
+                <div className="w-[1.5px] h-[100%] bg-white "></div>
+                
+                <div>
+                    <div>
+                        <div className="flex items-center">
+                            <div className="w-[100px] h-[100px] overflow-hidden object-cover relative ">
+                                <img src="https://i.ibb.co/ZRmL8Hgg/637cb9ba322617428493829d-1111.png" alt="menu" className="hover-image-menu-out" />
+                            </div>
+                            <p className="text-2xl pr-4">Chawli Beans and Mint Burger</p>
+                        </div>
+                    </div>
+                    <hr className="mt-2" />
+                    <div>
+                        <div className="flex items-center">
+                            <div className="w-[100px] h-[100px] overflow-hidden object-cover relative ">
+                                <img src="https://i.ibb.co/ZRmL8Hgg/637cb9ba322617428493829d-1111.png" alt="menu" className="hover-image-menu-out" />
+                            </div>
+                            <p className="text-2xl pr-4">Chawli Beans and Mint Burger</p>
+                        </div>
+                    </div>
+                    <hr className="mt-2" />
+                    <div>
+                        <div className="flex items-center">
+                            <div className="w-[100px] h-[100px] overflow-hidden object-cover relative ">
+                                <img src="https://i.ibb.co/ZRmL8Hgg/637cb9ba322617428493829d-1111.png" alt="menu" className="hover-image-menu-out" />
+                            </div>
+                            <p className="text-2xl pr-4">Chawli Beans and Mint Burger</p>
+                        </div>
+                    </div>
+                    <hr className="mt-2" />
+                    <div>
+                        <div className="flex items-center">
+                            <div className="w-[100px] h-[100px] overflow-hidden object-cover relative ">
+                                <img src="https://i.ibb.co/ZRmL8Hgg/637cb9ba322617428493829d-1111.png" alt="menu" className="hover-image-menu-out" />
+                            </div>
+                            <p className="text-2xl pr-4">Chawli Beans and Mint Burger</p>
+                        </div>
+                    </div>
+                    <hr className="mt-2" />
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/home/Visit.tsx
+++ src/components/home/Visit.tsx
@@ -3,7 +3,38 @@
 const Visit = () => {
     return (
         <div>
-            Visit
+            <h3>Visit Our Restaurant</h3>
+            <p>
+                Quality country-style menu selection, friendly and efficient service, combined with genuine value has kept Our Best serving families like yours for over 28.
+            </p>
+            <div>
+                <div>
+                    <div className="visit-image-div">
+                        <img src="https://i.ibb.co/nq4rK3bh/637dae75fb4b6d377f54614a-Rectangle-77.png" alt="our restaurant" 
+                        className="   " />
+                    </div>
+                    <div className="visit-image-div">
+                        <img src="https://i.ibb.co/4Zxh0CRb/637daea06029730be63878bc-Rectangle-80.png" alt="our restaurant" 
+                        className="   " />
+                    </div>
+                </div>
+                <div>
+                    <div className="visit-image-div">
+                        <img src="https://i.ibb.co/KjkRHKJR/637daeaf0c998d31d6755dd8-Rectangle-78.png" alt="our restaurant" 
+                        className="   " />
+                    </div>
+                </div>
+                <div>
+                    <div className="visit-image-div">
+                        <img src="https://i.ibb.co/q31ywz7s/637daec060297333a0387c05-Rectangle-79.png" alt="our restaurant" 
+                        className="   " />
+                    </div>
+                    <div className="visit-image-div">
+                        <img src="https://i.ibb.co/gMXH4gyn/637daed33e538d3989f24832-Rectangle-81.png" alt="our restaurant" 
+                        className="   " />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
