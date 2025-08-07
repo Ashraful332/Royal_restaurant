fc892415dca86c842b63058c92150a240a73dec6
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-08-07 12:44:08 +0600

    update the all the website is complite now responsive deisng and my copyrite free photo and text adding is is work

--- src/components/home/Newsletter.tsx
+++ src/components/home/Newsletter.tsx
@@ -2,8 +2,31 @@
 
 const Newsletter = () => {
     return (
-        <div>
-            Newsletter
+        <div className="mt-high mb-sec data-main  ">
+            <div className="flex flex-row items-center justify-center w-[60vw] max-w-[1170px] gap-[60%] mx-auto">
+                <div className="w-[22vw] ">
+                    <h2 className="text-white font-bold text-3xl">Newsletter</h2>
+                    <p>Subscribe to our newsletter and receive 15% discount from your order.</p>
+                </div>
+                <div className="form-control pr-4 ">
+                    <input type="text" required />
+                    <label>
+                        <span style={{ transitionDelay: '0ms' }}>E</span>
+                        <span style={{ transitionDelay: '50ms' }}>m</span>
+                        <span style={{ transitionDelay: '100ms' }}>a</span>
+                        <span style={{ transitionDelay: '150ms' }}>i</span>
+                        <span style={{ transitionDelay: '200ms' }}>l</span>
+                        <span style={{ transitionDelay: '250ms' }}> </span>
+                        <span style={{ transitionDelay: '300ms' }}>a</span>
+                        <span style={{ transitionDelay: '350ms' }}>d</span>
+                        <span style={{ transitionDelay: '400ms' }}>d</span>
+                        <span style={{ transitionDelay: '450ms' }}>r</span>
+                        <span style={{ transitionDelay: '500ms' }}>e</span>
+                        <span style={{ transitionDelay: '550ms' }}>s</span>
+                        <span style={{ transitionDelay: '600ms' }}>s</span>
+                    </label>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/home/Our_Blog.tsx
+++ src/components/home/Our_Blog.tsx
@@ -1,28 +1,52 @@
-
+import Arrow from "../../assets/image/Popular/arrow.svg";
+import "./home.css"
 
 const Our_Blog = () => {
     return (
-        <div>
-            <p>Our Blog</p>
-            <h3>Recent Articles</h3>
-            <div>
-                <div>
-                    <img src="" alt="food" />
+        <div className="flex-center mt-high max-w-[1170px] h-auto mx-auto pt-4">
+            <p className="text-[14px] ">Our Blog</p>
+            <h3 className="text-6xl font-bold mb-primary">Recent Articles</h3>
+            <div className="w-[60vw] flex flex-col lg:flex-row gap-10 mb-sec ">
+                <div className="w-[50%] mx-auto h-[357px] overflow-hidden object-center relative ">
+                    <img src="https://i.ibb.co.com/mFGtpR3m/63786d3454cc507817e7bd6c-1.png" alt="food" 
+                    className="hover-image-blog"/>
                 </div>
-                <div>
+                <div className="w-[50%] ">
                     <p>August 6, 2025</p>
-                    <h3>The Most Expensive Cup of Coffee in the Usa</h3>
-                    <p>
+                    <h3 className="text-white font-bold text-2xl mt-2 mb-4 ">The Most Expensive Cup of Coffee in the Usa</h3>
+                    <p className="text-[#ffffffef] ">
                         Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque lauatium, totam rem aperiam perspiciatis unde omnis.
                     </p>
-                    <div>
+                    <div className="flex items-center text-center justify-between w-[50%] mt-4 mb-6 text-sm">
                         <p>Comments 165</p>
                         <p>View 1265</p>
-                    </div>
-                    <button>Read Now</button>
+                    </div> 
+                    <button className="Read-Now">Read Now</button>
                 </div>
             </div>
-        </div>
+            <div className="w-[60vw] flex flex-col lg:flex-row-reverse gap-10 mb-sec ">
+                <div className="w-[50%] mx-auto h-[357px] overflow-hidden object-center relative ">
+                    <img src="https://i.ibb.co.com/84SbdhXT/63819251c50a511441758a4f-Rectangle-27.png" alt="food" 
+                    className="hover-image-blog"/>
+                </div>
+                <div className="w-[50%] ">
+                    <p>August 6, 2025</p>
+                    <h3 className="text-white font-bold text-2xl mt-2 mb-4 ">The Most Expensive Cup of Coffee in the Usa</h3>
+                    <p className="text-[#ffffffef] ">
+                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque lauatium, totam rem aperiam perspiciatis unde omnis.
+                    </p>
+                    <div className="flex items-center text-center justify-between w-[50%] mt-4 mb-6 text-sm">
+                        <p>Comments 165</p>
+                        <p>View 1265</p>
+                    </div> 
+                    <button className="Read-Now">Read Now</button>
+                </div>
+            </div>
+            <button className=" flex justify-center mx-auto Explore-blog pb-[var(--margin-primary)] ">
+                Explore All Food
+                <img src={Arrow} alt="star" className="ml-2" />
+            </button>
+        </div> 
     );
 };
 
