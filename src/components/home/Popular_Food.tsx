94020af61b7dea847d5797c0e85de616b7ae151b
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-08-08 12:27:49 +0600

    a

--- src/components/home/Popular_Food.tsx
+++ src/components/home/Popular_Food.tsx
@@ -5,12 +5,12 @@ import Arrow from "../../assets/image/Popular/arrow.svg";
 
 const Popular_Food = () => {
     return (
-        <div className="flex-center text-white max-w-[1170px] mx-auto ">
-            <h3 className="text-6xl font-bold mb-7">Most popular food</h3>
-            <p className="w-[35%] text-center mb-[50px] ">
+        <div className="flex-center text-white max-w-[1170px]  h-auto mx-auto pb-9 bg-amber-200 ">
+            <h3 className=" text-header mb-7 pt-16">Most popular food</h3>
+            <p className=" w-[80vw] sm:w-[60%] lg:w-[35%] text-center mb-[50px] ">
                 A list of most popular Bangladeshi food including mains, drinks, and deserts you must try while in Bangladesh, for an authentic experience. Check now!
             </p>
-            <div className="flex gap-7">
+            <div className="flex flex-col xl:flex-row gap-7">
                 <div className="w-[370px] ">
                     <div className="w-[370px] h-[300px] overflow-hidden object-center relative ">
                         <img src={Photo1} alt="photo 1" className="hover-image-popular " />
