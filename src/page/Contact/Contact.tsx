9256c970aed9d2b0b8f1233c7a48b9dd1d4919d8
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-08-10 15:10:55 +0600

    updaete the contact page

--- src/page/Contact/Contact.tsx
+++ src/page/Contact/Contact.tsx
@@ -1,9 +1,55 @@
+import Footer from "../../components/navigation/Footer";
+import Navbar from "../../components/navigation/Navbar";
 
 
 const Contact = () => {
     return (
         <div>
-            hello
+            <Navbar/>
+            <div>
+                <div>
+                    <div>
+                        <h3>Where to find us</h3>
+                        <p>
+                            The Patio Time Cafe is located on Silk Street, in the heart of London city, on the edge of Soho. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa
+                        </p>
+                        <p>
+                            The Patio Time Cafe is located on Silk Street, in the heart of London city, on the edge of Soho. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa
+                        </p>
+                        <div>
+                            <div>
+                                <img src="" alt="icon" />
+                                <p></p>
+                            </div>
+                            <div>
+                                <img src="" alt="icon" />
+                                <p></p>
+                            </div>
+                            <div>
+                                <img src="" alt="icon" />
+                                <p></p>
+                            </div>
+                        </div>
+                        <button>View in google maps</button>
+                    </div>
+                    <div>
+                        <img src="" alt="" />
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <h3>Contact Us</h3>
+                        <p>For general enquiries please email: info@gmail.com</p>
+                        <p>Reserve by email: gmail1234@gamil.com</p>
+                        <p>call:+8801614871378</p>
+                        <p>Opening hours:</p>
+                        <p>Mon - Thu: 10.00 am - 01:00 am</p>
+                        <p>Fri - Sun: 10:00 am - 02:00 am</p>
+                    </div>
+                    <div></div>
+                </div>
+            </div>
+            <Footer/>
         </div>
     );
 };
