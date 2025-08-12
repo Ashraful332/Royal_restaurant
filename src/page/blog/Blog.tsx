4572fcaf9129c3cf19af2e8c65ed541c0d97ba13
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-08-12 16:06:03 +0600

    add the blogpage some style

--- src/page/blog/Blog.tsx
+++ src/page/blog/Blog.tsx
@@ -1,22 +1,24 @@
 import Footer from "../../components/navigation/Footer";
 import Navbar from "../../components/navigation/Navbar";
-
+import "./blog.css"
 
 const Blog = () => {
     return (
-        <div>
+        <div className="text-primary max-w-screen overflow-x-hidden">
             <Navbar/>
-            <div>
+            <div className="flex-center max-w-[1170px] mx-auto px-[14px] sm:px-[30px] mt-sec ">
                 <div>
-                    <div>
+                    <div className="mb-sec">
                         <hr />
-                         <h2>Restaurant Management & Growth Blog</h2>
+                         <h2 className="text-header-blog my-[30px] ">Restaurant Management & Growth Blog</h2>
                         <hr />
                     </div>
-                    <div>
-                        <div></div>
+                    <div className="flex flex-col lg:flex-row gap-5">
                         <div>
-                            <h3>Everything you need to know about the growing role of AI in restaurants</h3>
+                            <img src="https://i.ibb.co.com/CsJCQp8h/63baf8d9f11099476361b130-Rectangle-600.png" alt="" />
+                        </div>
+                        <div className="w-[40%] flex flex-col gap-5 ">
+                            <h3 className="text-hed-introB ">Everything you need to know about the growing role of AI in restaurants</h3>
                             <p>August 6, 2022 . By Admin</p>
                             <p>You are sitting in your favorite restaurant and have ordered on a tablet on your table. A few seconds after placing the order, a notification will appear in your messaging app.</p>
                             <button>Read Now</button>
