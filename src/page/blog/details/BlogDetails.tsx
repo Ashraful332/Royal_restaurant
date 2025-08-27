108c10167e1dc61f4b980cc41fc1c3c8eef76de1
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-08-27 16:45:58 +0600

    add the data into the sertion

--- src/page/blog/details/BlogDetails.tsx
+++ src/page/blog/details/BlogDetails.tsx
@@ -1,11 +1,45 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 
 
+const AdminUrl = import.meta.env.VITE_ADMIN_URL;
+
+type BlogData = {
+    _id: string,
+    title: string,
+    SortDes: string,
+    photoUrl: string,
+    description: string,
+    View: string
+
+}
+
+
 export default function BlogDetails() {
-    const { id } = useParams();
+    const [blogs,setBlogs] = useState<BlogData[]>([])
+    const { id } = useParams(); // fecht the id of blog
+    
+    // fetch the data
+    useEffect(()=>{
+        async function LoadData() {
+           const response = await axios.get(`${AdminUrl}`,{id}) 
+           setBlogs(response.data)
+        }
+        LoadData()
+    },[])
+
     return(
         <div>
-            {id}
+            {blogs.map((blog)=>(
+                <div key={blog._id}>
+                    <img src={blog.photoUrl} alt="Blog image" />
+                    <p>{blog.title}</p>
+                    <p>{blog.SortDes}</p>
+                    <p>{blog.description}</p>
+                    <p>{blog.View}</p>
+                </div>
+            ))}
         </div>
     )
 }
