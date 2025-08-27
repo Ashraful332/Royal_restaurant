980f9cff3a1b145cf31c284a7ae56026e25825d6
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-08-27 17:26:46 +0600

    update the section

--- src/page/blog/details/BlogDetails.tsx
+++ src/page/blog/details/BlogDetails.tsx
@@ -17,14 +17,16 @@ type BlogData = {
 
 
 export default function BlogDetails() {
-    const [blogs, setBlogs] = useState<BlogData[]>([])
+    const [blogs, setBlogs] = useState<BlogData | any >(" ")
     const { id } = useParams(); // fecht the id of blog
 
     // fetch the data
     useEffect(() => {
         async function LoadData() {
             try {
-                const response = await axios.post(`${AdminUrl}`, { id })
+                const response = await axios.post(`${AdminUrl}/blog-details`, { id })
+                // const response = await axios.get(`${AdminUrl}/blog-details/${id}`)
+
                 setBlogs(response.data)
                 console.log(response.data);
             } catch (error) {
@@ -32,19 +34,17 @@ export default function BlogDetails() {
             }
         }
         LoadData()
-    }, [])
+    }, [id])
 
     return (
         <div>
-            {blogs.map((blog) => (
-                <div key={blog._id}>
-                    <img src={blog.photoUrl} alt="Blog image" />
-                    <p>{blog.title}</p>
-                    <p>{blog.SortDes}</p>
-                    <p>{blog.description}</p>
-                    <p>{blog.View}</p>
-                </div>
-            ))}
+            <div >
+                <img src={blogs.photoUrl} alt="Blog image" />
+                <p>{blogs.title}</p>
+                <p>{blogs.SortDes}</p>
+                <p>{blogs.description}</p>
+                <p>{blogs.View}</p>
+            </div>
         </div>
     )
 }
